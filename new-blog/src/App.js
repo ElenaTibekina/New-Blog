fe8adf83a806a812a134cb07880a3f92bff115f7
Elena Tibekina <elentibekina@gmail.com>
2019-04-06 15:21:15 +0300

    ten elements with css

--- new-blog/src/App.js
+++ new-blog/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import blogs from './blogs.js';
+import './blogs.css';
 
 
 class App extends Component {
@@ -9,8 +10,11 @@ class App extends Component {
       <div className="App">
         {blogs.items.map((blog) => {
           return (
-            <div>
-              {blog.title}
+            <div className="blog-item">
+              <a href="#p" className="title"><h2>{blog.title}</h2></a>
+              <a href="#p"><span className="author-name"><h3>{blog.author.name}</h3></span></a>
+              <span className="author-avatar">{blog.author.avatar}</span>
+              <article className="article">{blog.article}</article>
             </div>
           )
         })}
